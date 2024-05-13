d8e95e0b3dc6108c96ad3f0ac8bae54f1f97d7c6
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-05-13 14:32:51 -0300

    alterando functions de validação de dados em leadtech.sql

--- leadtech.sql
+++ leadtech.sql
@@ -357,63 +357,108 @@ END;
 
 -- FUNÇÕES DE VALIDAÇÃO DE DADOS
 
--- Primeira função para validar entrada de dados: idade do cliente
-/* Esta função valida se a idade do cliente é maior ou igual a 18 anos, 
+-- Primeira função para validar entrada de dados: tabela cliente
+/* Esta função valida os dados do cliente referente a colunas :
+    - idade: se é maior que 18 anos
+    - email : se contem o @
+    - genero : se é do tipo 'Feminino' ou 'Masculino'
 como requisito para cadastro, conforme estabelecido pela regra de negócio.*/
-DROP FUNCTION validar_idade_cliente;
-CREATE OR REPLACE FUNCTION validar_idade_cliente(idade_cliente NUMBER) RETURN BOOLEAN IS
+-- Drop a function se existir
+DROP FUNCTION validar_cliente;
+-- criando function para validar dados
+CREATE OR REPLACE FUNCTION validar_cliente(
+    idade_cliente NUMBER,
+    email_cliente VARCHAR2,
+    genero_cliente VARCHAR2
+) RETURN BOOLEAN IS
+    idade_valida BOOLEAN := TRUE;
+    email_valido BOOLEAN := TRUE;
+    genero_valido BOOLEAN := TRUE;
 BEGIN
-    IF idade_cliente >= 18 THEN
-        RETURN TRUE;
-    ELSE
-        RETURN FALSE;
+    -- Validando idade
+    IF idade_cliente < 18 THEN
+        idade_valida := FALSE;
     END IF;
-END;
 
--- Segunda funcao para validar entrada de dados 
-/* Esta função valida se o valor do produto é maior que zero, 
-garantindo que apenas valores válidos sejam inseridos no banco de dados.*/
-DROP FUNCTION validar_valor_produto;
-CREATE OR REPLACE FUNCTION validar_valor_produto(valor_produto NUMBER) RETURN BOOLEAN IS
-BEGIN
-    IF valor_produto > 0 THEN
-        RETURN TRUE;
-    ELSE
-        RETURN FALSE;
+    -- Validando Email
+    IF INSTR(email_cliente, '@') = 0 THEN
+        email_valido := FALSE;
     END IF;
-END;
 
--- Bloco PL/SQL com exemplo de uso das funções de validação
+    -- Validando genero
+    IF genero_cliente NOT IN ('Feminino', 'Masculino') THEN
+        genero_valido := FALSE;
+    END IF;
+
+    RETURN idade_valida AND email_valido AND genero_valido;
+END;
+-- Bloco PL/SQL com exemplo de chamada validar_cliente
 DECLARE
-    v_idade_cliente cliente.idade%TYPE := &idadeCliente;
-    v_valor_produto produto.valorproduto%TYPE := &valorProduto;
+    idade_cliente NUMBER := 25;
+    email_cliente VARCHAR2(255) := 'cliente@example.com';
+    genero_cliente VARCHAR2(10) := 'Masculino';
+    resultado_validacao BOOLEAN;
 BEGIN
-    IF v_idade_cliente < 18 THEN
-        DBMS_OUTPUT.PUT_LINE('A idade do cliente não é válida.');
+    resultado_validacao := validar_cliente(idade_cliente, email_cliente, genero_cliente);
+    
+    IF resultado_validacao THEN
+        DBMS_OUTPUT.PUT_LINE('Os detalhes do cliente são válidos.');
     ELSE
-        BEGIN
-            INSERT INTO Cliente (idCliente, nome, telefone, email, idade, genero, estadoCivil, idLocalizacao, nivelRenda, nivelEducacao, formaPagamentoPref) 
-            VALUES (1, 'Maria', '123456789', 'maria@maria.com', v_idade_cliente, 'Feminino', 'Casado(a)', 201, 1000.00, 'Fundamental', 'Boleto');
-        EXCEPTION
-            WHEN OTHERS THEN
-                DBMS_OUTPUT.PUT_LINE('Ocorreu um erro ao inserir dados do cliente.');
-        END;
+        DBMS_OUTPUT.PUT_LINE('Os detalhes do cliente são inválidos.');
     END IF;
-
-    IF v_valor_produto <= 0 THEN
-        DBMS_OUTPUT.PUT_LINE('O valor do produto não é válido.');
-    ELSE
-        BEGIN
-            INSERT INTO Produto (idProduto, nomeProduto, estrelas, categoriaProduto, qtdEstoque, dataCompraProduto, valorProduto) 
-            VALUES (1, 'Maquiagem', 5, 'Cosméticos', 10, SYSDATE, v_valor_produto);
-        EXCEPTION
-            WHEN OTHERS THEN
-                DBMS_OUTPUT.PUT_LINE('Ocorreu um erro ao inserir dados do produto.');
-        END;
-    END IF;
-    COMMIT;
 END;
 
+
+-- Segunda funcao para validar entrada de dados: tabela produto
+/* Esta função valida os dados do produto referente a colunas :
+    - estrelas:  estar entre 1 ate 5
+    - estoque : se é um numero positivo
+    - valor do produto : se é numero positivo
+garantindo que apenas valores válidos sejam inseridos no banco de dados.*/
+DROP FUNCTION validar_produto;
+CREATE OR REPLACE FUNCTION validar_produto(
+    estrelas NUMBER,
+    qtd_estoque NUMBER,
+    valor_produto NUMBER
+) RETURN BOOLEAN IS
+    estrelas_validas BOOLEAN := TRUE;
+    estoque_valido BOOLEAN := TRUE;
+    valor_valido BOOLEAN := TRUE;
+BEGIN
+    -- Validar se o número de estrelas está entre 1 e 5
+    IF estrelas IS NULL OR estrelas < 1 OR estrelas > 5 THEN
+        estrelas_validas := FALSE;
+    END IF;
+
+    -- Validar se a quantidade em estoque é um número positivo
+    IF qtd_estoque IS NULL OR qtd_estoque < 0 THEN
+        estoque_valido := FALSE;
+    END IF;
+
+    -- Validar se o valor do produto é um número positivo
+    IF valor_produto IS NULL OR valor_produto < 0 THEN
+        valor_valido := FALSE;
+    END IF;
+
+    -- Retornar verdadeiro se todos os critérios forem atendidos, falso caso contrário
+    RETURN estrelas_validas AND categoria_valida AND estoque_valido AND valor_valido;
+END;
+-- Bloco PL/SQL com exemplo de chamada validar_produto
+DECLARE
+    estrelas_produto NUMBER := 10;
+    categoria_produto VARCHAR2(100) := 'Eletrônicos';
+    qtd_estoque NUMBER := 100;
+    valor_produto NUMBER := 1500.00;
+    resultado_validacao BOOLEAN;
+BEGIN
+    resultado_validacao := validar_produto(estrelas_produto, categoria_produto, qtd_estoque, valor_produto);
+    
+    IF resultado_validacao THEN
+        DBMS_OUTPUT.PUT_LINE('Os detalhes do produto são válidos.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Os detalhes do produto são inválidos.');
+    END IF;
+END;
 
 
 -- CRIAÇÃO DE PROCEDURES DAS TABELAS => INSERT, DELETE E UPDATE
