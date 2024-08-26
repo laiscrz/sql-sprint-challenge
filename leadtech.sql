cdbc53859e07dac3100444679d11e01bf0bfa460
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-08-26 12:47:28 -0300

    Update leadtech.sql rename class

--- leadtech.sql
+++ leadtech.sql
@@ -6,9 +6,9 @@ Banco de dados relacionado ao projeto "Conversao Inteligente"
 
 Bianca Leticia Román Caldeira - RM552267 - Turma : 2TDSPH
 Charlene Aparecida Estevam Mendes Fialho - RM552252 - Turma : 2TDSPH
-Lucca Raphael Pereira dos Santos - RM 99675 - Turma : 2TDSPW
 Lais Alves Da Silva Cruz - RM:552258 - Turma : 2TDSPH
 Fabrico Torres Antonio - RM:97916 - Turma : 2TDSPH
+Lucca Raphael Pereira dos Santos - RM 99675 - Turma : 2TDSPZ
 */
 
 -- Esses comandos exclui se a tabela ja existir, e tambem exclui
