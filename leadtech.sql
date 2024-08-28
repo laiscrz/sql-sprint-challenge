845d495375ab44cdde28fe466e6cba537ed58bea
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-08-28 16:22:54 -0300

    Update leadtech.sql

--- leadtech.sql
+++ leadtech.sql
@@ -8,7 +8,7 @@ Bianca Leticia Román Caldeira - RM552267 - Turma : 2TDSPH
 Charlene Aparecida Estevam Mendes Fialho - RM552252 - Turma : 2TDSPH
 Lais Alves Da Silva Cruz - RM:552258 - Turma : 2TDSPH
 Fabrico Torres Antonio - RM:97916 - Turma : 2TDSPH
-Lucca Raphael Pereira dos Santos - RM 99675 - Turma : 2TDSPZ
+Lucca Raphael Pereira dos Santos - RM 99675 - Turma : 2TDSPZ -> PROFESSOR: Milton Goya
 */
 
 -- Esses comandos exclui se a tabela ja existir, e tambem exclui
